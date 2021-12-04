c67bb2e4878971db26b0ef43fc67481af917762a
manny2trill <manuelr42787@gmail.com>
2021-12-03 23:03:17 -0600

    comment line 5

--- rml/ratemylawyer/views.py
+++ rml/ratemylawyer/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def ratemylawyer(request):
+# mylawyer(request):
 #     if request.method == 'GET':
 #         return render(request = request,
-#                     template_name = 'main.html')
+#                     template_name = 'main.html')# def rate
 def main(request):
     return render(request, 'main.html', {})
