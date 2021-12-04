f15e73cfbb84ff37aaa7714627454b59a8d1b709
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-12-03 20:59:02 -0800

    created contact.html

--- rml/ratemylawyer/views.py
+++ rml/ratemylawyer/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def ratemylawyer(request):
-#     if request.method == 'GET':
-#         return render(request = request,
-#                     template_name = 'main.html')
+def ratemylawyer(request):
+    if request.method == 'GET':
+        return render(request = request,
+                    template_name = 'main.html')
 def main(request):
-    return render(request, 'main.html', {})
+    return render(request, 'main.html')
+     
