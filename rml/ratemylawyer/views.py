2e128c8e0a29ce71ecae4a48a590929c234d5d5a
manny2trill <manuelr42787@gmail.com>
2021-12-07 15:19:48 -0600

    fixed  errors

--- rml/ratemylawyer/views.py
+++ rml/ratemylawyer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-# from .models import Lawyer
+from .models import Lawyer
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-# from .forms import EditorForm
+from .forms import EditorForm
+# from .models import Datetime
 
 
 
@@ -15,31 +16,31 @@ from django.urls import reverse
 # >>>>>>> main
 def main(request):
     # lawyers = Lawyer.objects.all().order_by('-lawyer_id')
-    now = datetime.now()
-    current_year = now.year
-    return render(request, 'main.html',{'current_year': current_year})
+    # now = datetime.now()
+    # current_year = now.year
+    return render(request, 'main.html')
 
-# def create(request, specialty_id):
-#     if request.method == 'GET':
-#         form = EditorForm()
-#         return render(request=request, template_name='create.html', context={ 'form': form, 'id': specialty_id })
+def create(request, specialty_id):
+    if request.method == 'GET':
+        form = EditorForm()
+        return render(request=request, template_name='create.html', context={ 'form': form, 'id': specialty_id })
 
-#     if request.method == 'POST':
-#         form = EditorForm(request.post)
-#         if form.is_valid():
-#             if 'create' in request.POST:
-#                 # get cleaned data from form
-#                 name = form.cleaned_data['name']
-#                 email = form.cleaned_data['email']
-#                 address = form.cleaned_data['address']
-#                 phone = form.cleaned_data['phone']
-#                 license = form.cleaned_data['license']
-#                 # create QuerySet object with cleaned name, specialty, address, phone
-#                 lawyer = Lawyer.objects.create(name=name, email=email, address=address, phone=phone, license=license)
-#                 # set cleaned tags to ManyRelatedManager object
-#                 lawyer.specialty.set(specialty)
-#             submit = form.cleaned_data['lawyer']
-#         return HttpResponseRedirect(reverse('lawyer'))
+    if request.method == 'POST':
+        form = EditorForm(request.post)
+        if form.is_valid():
+            if 'create' in request.POST:
+                # get cleaned data from form
+                name = form.cleaned_data['name']
+                email = form.cleaned_data['email']
+                address = form.cleaned_data['address']
+                phone = form.cleaned_data['phone']
+                license = form.cleaned_data['license']
+                # create QuerySet object with cleaned name, specialty, address, phone
+                lawyer = Lawyer.objects.create(name=name, email=email, address=address, phone=phone, license=license)
+                # set cleaned tags to ManyRelatedManager object
+                lawyer.specialty.set(specialty)
+            submit = form.cleaned_data['lawyer']
+        return HttpResponseRedirect(reverse('lawyer'))
 
      
      
