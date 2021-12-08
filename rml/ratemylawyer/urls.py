ff55500a7c3d4aa70f196d8a8cb3721ba2aefb91
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-12-08 14:43:22 -0800

    media/urls

--- rml/ratemylawyer/urls.py
+++ rml/ratemylawyer/urls.py
@@ -11,10 +11,7 @@ urlpatterns = [
     path('create/<int:specialty_id>', views.create, name="create"),
     path('edit/<int:specialty_id>', views.edit, name='edit'),
     path('browselawyer/', views.browse, name="browse"),
-<<<<<<< HEAD
+    path('media/', views.media, name="media"),
+]
+    
 
-]
-=======
-    path('media/', views.media, name="media"),   
-]
->>>>>>> 842c9c9307d705ce836af5f95c5a205cd6e4ebde
