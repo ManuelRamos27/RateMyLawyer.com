9b968d421bce62533af0e200673af311b477583d
sterno2510 <sterno2510@gmail.com>
2021-12-11 11:46:22 -0500

    new build

--- rml/ratemylawyer/forms.py
+++ rml/ratemylawyer/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 from .models import Lawyer
-from .models import Specialty
+#from .models import Specialty
 
 class EditorForm(forms.Form):
     name = forms.CharField(max_length=255, required=True)
-    #specialty = forms.CharField(max_length=255, required=True)
     address = forms.CharField(max_length=255, required=True)
     phone = forms.CharField(max_length=255, required=True)
     email = forms.EmailField(max_length=255, required=True)
     license = forms.CharField(max_length=255, required=True)
-    #cost = forms.IntegerField()
-    #comment = forms.ForeignKey(Review, blank=True, null=True, on_delete=forms.CASCADE)
-    #image = forms.ImageField(upload_to='images/')
+    cost = forms.IntegerField()
     website = forms.URLField(max_length=200)
-    #guests = forms.ManyToManyField(Guests, blank=True)
-    # specialty = forms.ManyToManyField('Specialty', blank=True)
+    rating = forms.IntegerField()
     choices = []
-    for specialty in Specialty.objects.all():
-        choices.append((specialty.specialty_id, specialty.name))
-    specialties = forms.MultipleChoiceField(choices=choices, widget=forms.CheckboxSelectMultiple, required=True)
+    # for specialty in Specialty.objects.all():
+    #     choices.append((specialty.specialty_id, specialty.name))
+    # specialties = forms.MultipleChoiceField(choices=choices, widget=forms.CheckboxSelectMultiple, required=True)
