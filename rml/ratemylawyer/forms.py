8db60c570faf815adf2a75570483d67111dbb8d1
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-12-14 18:39:32 -0800

    new forms.py for specialties

--- rml/ratemylawyer/forms.py
+++ rml/ratemylawyer/forms.py
@@ -17,9 +17,13 @@ class EditorForm(forms.Form):
     rating = forms.CharField(label='How many stars would you give this lawyer?', widget=forms.Select(choices=star_choices))
     cost = forms.CharField(label='How would you classify cost?', widget=forms.Select(choices=cost_choices))
     choices = []
+
     for specialty in Specialty.objects.all():
         choices.append((specialty.specialty_id, specialty.name))
-    specialties = forms.MultipleChoiceField(choices=choices, widget=forms.CheckboxSelectMultiple, required=True)
+    print(choices)
+  
+
+    specialties = forms.MultipleChoiceField(label = "specialties", choices=choices, widget=forms.CheckboxSelectMultiple, required=True)
 
 # class MyModel(models.Model):
 #     start_time = models.DateFimeField(null=True, blank=True)
